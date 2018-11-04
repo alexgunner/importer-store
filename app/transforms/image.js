308c743ac21a64110e723d00b49c878d881edba9
DharaRioja <cuatita_16@hotmail.com>
2018-11-03 23:46:47 -0400

    host de prueba

--- app/transforms/image.js
+++ app/transforms/image.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Transform.extend({
   deserialize(serialized) {
     //return "http://todoconstruccion-api.herokuapp.com" + serialized;
-    return "http://localhost:3000/" + serialized;
+    return "http://api.domusbolivia.com/" + serialized;
   },
 
   serialize(deserialized) {
