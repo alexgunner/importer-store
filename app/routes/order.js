94270afe78f73801e3fb8da12d6cfe2574d81346
DharaRioja <cuatita_16@hotmail.com>
2018-11-20 01:29:04 -0400

    Recupera y guarda datos de cliente registrado para la orden

--- app/routes/order.js
+++ app/routes/order.js
@@ -1,11 +1,14 @@
 import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
+const { service } = Ember.inject;
 var id;
 export default Route.extend({
+    session: service('session'),
     model() {
        return RSVP.hash({
          host: this.get('store').adapterFor('application').get('host'),
          deliveries: this.get('store').findAll('delivery'),
+         items: this.get('store').findAll('item')
         })
      },
      actions: {
@@ -26,20 +29,57 @@ export default Route.extend({
         var pay = document.querySelector('#pagos');
         var delivery = document.querySelector('#opcion')
         var select = document.querySelector("#deliveries");
-      //   const client =  null;
-      //   if (c_name != null){
-      //       const client = store.createRecord('client', {
-      //         name: c_name,
-      //         lastame: c_lastname,
-      //         nit: c_nit,
-      //         address: c_address,
-      //         phone: c_phone,
-      //         mail: c_mail
-      //       })
-      //   }
-      //   else{
-      const client = this.get('store').createRecord('client', this.currentModel);
-        
+        if(this.get('session.isAuthenticated'))
+        {
+          const client = store.createRecord('client', {
+            name: this.get('session.data.authenticated.name'),
+            lastame: this.get('session.data.authenticated.lastname'),
+            nit: this.get('session.data.authenticated.nit'),
+            address: this.get('session.data.authenticated.address'),
+            phone: this.get('session.data.authenticated.phone'),
+            mail: this.get('session.data.authenticated.email')
+            })
+            console.log("entro al action");
+            client.save().then(function(record){
+              console.log("hizo el save");
+              var order = store.createRecord('order', {
+                orderdate: new Date(),
+                client_id: record.id,
+                delivery_id: select.value,
+                typepay: pay.value,
+                typedelivery: delivery.value
+              });
+              console.log("crea cliente");
+                order.save().then(function(record){
+                  items.forEach(function(item){
+                    var cart = store.createRecord('cart', {
+                      quantity: item.get('quantity'),
+                      order_id: record.id,
+                      product_variant_id: item.get('variant_id'),
+                      role: item.get('role')
+                    });
+                    console.log("creo todo");
+                    cart.save();
+                  });
+                id = record.id
+                console.log(id);
+              });
+             
+              });
+              swal({
+                title: "¡Hecho!",
+                text: "Tu orden fue registrada, por continua a proceder con el pago.",
+                type: "success",
+                confirmButtonText: "OK"
+              },
+              function(isConfirm){
+                if (isConfirm) {
+                  window.location.href = '/pay/'+ id;
+                }
+              });
+        }
+      else{
+        const client = this.get('store').createRecord('client', this.currentModel);
         console.log("entro al action");
         client.save().then(function(record){
           console.log("hizo el save");
@@ -78,6 +118,8 @@ export default Route.extend({
               window.location.href = '/pay/'+ id;
             }
           });
+      }
+        
       },
       cancel() {
         this.transitionTo('/');
