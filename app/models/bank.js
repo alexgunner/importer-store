91f82da76dd48b13336e57ce3228fef9394a0d54
DharaRioja <cuatita_16@hotmail.com>
2018-12-26 22:25:17 -0400

    banco muestra mas datos

--- app/models/bank.js
+++ app/models/bank.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr(),
-	acount: DS.attr(),
+    acount: DS.attr(),
+    nameaccount: DS.attr(),
+    typemoney: DS.attr()
 });
