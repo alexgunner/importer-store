ffe66df9d88719745a6133cb9a9fb8bb7f3ee78f
DharaRioja <cuatita_16@hotmail.com>
2019-01-04 23:44:34 -0400

    arreglos en css

--- app/components/navigation-bar.js
+++ app/components/navigation-bar.js
@@ -4,11 +4,18 @@ export default Component.extend({
     session: service('session'),
     actions: {
         logout(items) {
-        items.forEach(function(item){
-            item.deleteRecord();
-            item.save();
-        })
-        this.get('session').invalidate();
+            items.forEach(function(item){
+                item.deleteRecord();
+                item.save();
+            })
+            this.get('session').invalidate();
+        },
+
+        searchProduct() {
+            var productname = this.get('name').toLowerCase();
+            productname = productname.replace(/ /g, "%20" );
+            console.log("El nombre es:", productname);
+            //this.sendAction('afterSearch', productname);
         }
     }
 });
