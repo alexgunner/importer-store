bf4e79bb5404cd6f0d3694020577b5c9faf66d32
DharaRioja <cuatita_16@hotmail.com>
2019-02-01 12:15:34 -0400

    cambio de loader

--- app/routes/history.js
+++ app/routes/history.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 
 export default Route.extend({
-    model(params) {
+    model() {
 		return RSVP.hash({
 			orders: this.get('store').findAll('order'),
 			product_variants: this.get('store').findAll('product_variant')
