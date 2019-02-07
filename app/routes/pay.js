28fb9f26fd7430f7e5c8b51c2c61b88aa37442f9
DharaRioja <cuatita_16@hotmail.com>
2019-02-07 17:15:06 -0400

    cambio menor

--- app/routes/pay.js
+++ app/routes/pay.js
@@ -95,6 +95,35 @@ export default Route.extend({
                   window.location.href = '/';
                 }
               });
+        },
+        pruebaDivAPdf() {
+            var pdf = new jsPDF('p', 'pt', 'letter');
+            var source = $('#imprimir')[0];
+    
+            var specialElementHandlers = {
+                '#bypassme': function (element, renderer) {
+                    return true
+                }
+            };
+            var margins = {
+                top: 80,
+                bottom: 60,
+                left: 40,
+                width: 522
+            };
+    
+            pdf.fromHTML(
+                source, 
+                margins.left, // x coord
+                margins.top, { // y coord
+                    'width': margins.width, 
+                    'elementHandlers': specialElementHandlers
+                },
+    
+                function (dispose) {
+                    pdf.save('Prueba.pdf');
+                }, margins
+            );
         }
     }
 });
