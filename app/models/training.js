876a48c5cd640bde7fc560f0266830e5bf2fad0d
DharaRioja <cuatita_16@hotmail.com>
2019-02-11 16:43:34 -0400

    cambios trainings

--- app/models/training.js
+++ app/models/training.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr(),
     description: DS.attr(),
-    url: DS.attr()
+    url: DS.attr(),
+    videolink: DS.attr()
 });
